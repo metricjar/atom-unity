61b50b28fd2bf06fa767d1ec36ce5fd051f1f8a7
Valentine.Pavchuk <valentine.pavchuk@Admins-MacBook-Pro-5.local>
2016-05-30 13:25:34 +0300

    added travis sh scripts

---
+++ generate_documentation.sh
@@ -0,0 +1,27 @@
+mkdir doc && doxygen doxyfile
+
+TARGET_BRANCH="gh-pages"
+mkdir $TARGET_BRANCH && cd $TARGET_BRANCH 
+git clone -b $TARGET_BRANCH --single-branch https://github.com/ironSource/atom-unity.git
+cp -r ../doc/* ./atom-unity
+
+REPO_COMMIT_AUTHOR=$(git show -s --pretty=format:"%cn")
+REPO_COMMIT_AUTHOR_EMAIL=$(git show -s --pretty=format:"%ce")
+git config user.name "$REPO_COMMIT_AUTHOR"
+git config user.email "$REPO_COMMIT_AUTHOR_EMAIL"
+
+cd atom-unity
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Now that we're all set up, we can push.
+git push $origin $TARGET_BRANCH
