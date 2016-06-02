57763f1e14fcbb429671c2bf1601a0e4b48c5d0d
g8y3e <valentine.pavchuk@ironsrc.com>
2016-06-02 14:20:13 +0300

    fixed bulk test

--- atom-sdk/test/BulkDataTest.cs
+++ atom-sdk/test/BulkDataTest.cs
@@ -48,6 +48,26 @@ namespace ironsource {
 
                 Assert.AreEqual(expectedSize, data.GetSize());
             }
+
+            [Test]
+            public void TestGetData() {
+                BulkData data = new BulkData();
+
+                data.AddData("1");
+                data.AddData("2");
+                data.AddData("3");
+
+                List<string> expectedList = new List<string>();
+                expectedList.Add("1");
+                expectedList.Add("2");
+                expectedList.Add("3");
+
+                List<string> compareList = data.GetData();
+
+                for (int i = 0; i < expectedList.Count; ++i) {
+                    Assert.AreEqual(expectedList[i], compareList[i]);
+                }
+            }
 		}
 	}
 }
